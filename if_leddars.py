92c534b11e8ecf292f227e9d0fdd4e5764826d51
Kishor Jena <ksr.jena@gmail.com>
2019-12-27 14:57:58 +0530

    Add files via upload

---
+++ if_leddars.py
@@ -0,0 +1,17 @@
+# If-elif-else program
+
+a = 10
+b = 20
+c = 30
+
+if a>b:
+	if a>c:
+		print(a,"is largest")
+elif b>a:
+	if b>c:
+		print(b,"is largest")
+	else:
+		print(c,"c is largest")
+
+
+
---
+++ nested_if.py
@@ -0,0 +1,14 @@
+# If-elif-else program
+
+a = 10
+b = 20
+c = 30
+
+if a>b and a>c:
+	print(a,"is largest")
+elif b>a and a>c:
+	print(b,"is largest")
+else:
+	print(c,"is largest")
+
+
